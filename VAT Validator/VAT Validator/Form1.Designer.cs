359aa4d7a58a054e14e293b7ccbf6a498834ed95
popedy <43247353+popedy@users.noreply.github.com>
2018-09-19 15:13:58 +0200

    Add files via upload

---
+++ VAT Validator/VAT Validator/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace VAT_Validator
+{
+    partial class VAT_Validator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VAT_Validator));
+            this.lst_ausgabe = new System.Windows.Forms.ListBox();
+            this.txt_eingabe = new System.Windows.Forms.TextBox();
+            this.btn_import = new System.Windows.Forms.Button();
+            this.axAcroPDF1 = new AxAcroPDFLib.AxAcroPDF();
+            this.txt_validate = new System.Windows.Forms.TextBox();
+            this.btn_validate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lst_ausgabe
+            // 
+            this.lst_ausgabe.FormattingEnabled = true;
+            this.lst_ausgabe.Location = new System.Drawing.Point(12, 76);
+            this.lst_ausgabe.Name = "lst_ausgabe";
+            this.lst_ausgabe.Size = new System.Drawing.Size(172, 290);
+            this.lst_ausgabe.TabIndex = 0;
+            this.lst_ausgabe.SelectedIndexChanged += new System.EventHandler(this.lst_ausgabe_SelectedIndexChanged);
+            this.lst_ausgabe.MouseLeave += new System.EventHandler(this.lst_ausgabe_MouseDoubleClick);
+            // 
+            // txt_eingabe
+            // 
+            this.txt_eingabe.Location = new System.Drawing.Point(12, 20);
+            this.txt_eingabe.Name = "txt_eingabe";
+            this.txt_eingabe.Size = new System.Drawing.Size(172, 20);
+            this.txt_eingabe.TabIndex = 1;
+            // 
+            // btn_import
+            // 
+            this.btn_import.AutoEllipsis = true;
+            this.btn_import.Location = new System.Drawing.Point(109, 46);
+            this.btn_import.Name = "btn_import";
+            this.btn_import.Size = new System.Drawing.Size(75, 23);
+            this.btn_import.TabIndex = 2;
+            this.btn_import.Text = "Import";
+            this.btn_import.UseVisualStyleBackColor = true;
+            this.btn_import.Click += new System.EventHandler(this.btn_import_Click);
+            // 
+            // axAcroPDF1
+            // 
+            this.axAcroPDF1.Enabled = true;
+            this.axAcroPDF1.Location = new System.Drawing.Point(193, 20);
+            this.axAcroPDF1.Name = "axAcroPDF1";
+            this.axAcroPDF1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axAcroPDF1.OcxState")));
+            this.axAcroPDF1.Size = new System.Drawing.Size(443, 514);
+            this.axAcroPDF1.TabIndex = 3;
+            this.axAcroPDF1.Enter += new System.EventHandler(this.axAcroPDF1_Enter);
+            // 
+            // txt_validate
+            // 
+            this.txt_validate.Location = new System.Drawing.Point(12, 387);
+            this.txt_validate.Multiline = true;
+            this.txt_validate.Name = "txt_validate";
+            this.txt_validate.Size = new System.Drawing.Size(172, 193);
+            this.txt_validate.TabIndex = 4;
+            // 
+            // btn_validate
+            // 
+            this.btn_validate.Location = new System.Drawing.Point(190, 547);
+            this.btn_validate.Name = "btn_validate";
+            this.btn_validate.Size = new System.Drawing.Size(74, 33);
+            this.btn_validate.TabIndex = 5;
+            this.btn_validate.Text = "Validate VAT";
+            this.btn_validate.UseVisualStyleBackColor = true;
+            this.btn_validate.Click += new System.EventHandler(this.btn_validate_Click);
+            // 
+            // VAT_Validator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 592);
+            this.Controls.Add(this.btn_validate);
+            this.Controls.Add(this.txt_validate);
+            this.Controls.Add(this.axAcroPDF1);
+            this.Controls.Add(this.btn_import);
+            this.Controls.Add(this.txt_eingabe);
+            this.Controls.Add(this.lst_ausgabe);
+            this.Name = "VAT_Validator";
+            this.Text = "VAT Validator";
+            this.Load += new System.EventHandler(this.VAT_Validator_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lst_ausgabe;
+        private System.Windows.Forms.TextBox txt_eingabe;
+        private System.Windows.Forms.Button btn_import;
+        private AxAcroPDFLib.AxAcroPDF axAcroPDF1;
+        private System.Windows.Forms.TextBox txt_validate;
+        private System.Windows.Forms.Button btn_validate;
+    }
+}
+
---
+++ VAT Validator/VAT Validator/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System.ServiceModel;
+
+namespace VAT_Validator
+{
+    public partial class VAT_Validator : Form
+    { 
+        
+        string pfad = ""; //speicher für den ersten Pfad
+        string name;     //speicher für die Name der Datei
+        string vpfad;   //speicher für den Pfad der Datei
+        StringBuilder auslesen = new StringBuilder(); //Speicher für das aus der PDF Datei extrahiertes Text.
+       
+        //string firmenname, adresse; // 
+        //bool valid;
+        
+
+
+
+
+
+        public VAT_Validator()
+        {
+            InitializeComponent();
+
+
+        }
+       
+        private void btn_import_Click(object sender, EventArgs e)
+        {
+            lst_ausgabe.Items.Clear();
+            
+            DirectoryInfo filefolder = new DirectoryInfo(txt_eingabe.Text);//Directory eingeben und dessen pdf* files in die Textbox eingeben.
+            if (filefolder.Exists)
+            {
+                
+            FileInfo[] Files = filefolder.GetFiles("*.pdf");
+            pfad = txt_eingabe.Text;
+
+            foreach (FileInfo fn in Files) //Pfad Prüfen
+            {
+
+                lst_ausgabe.Items.Add(fn);
+
+            }
+            }
+            else { MessageBox.Show("Bitte geben Sie ein gültiges Pfad ein!"); }
+                 }
+        private void axAcroPDF1_Enter(object sender, EventArgs e)//preview
+        {
+            
+        }
+
+        private void lst_ausgabe_SelectedIndexChanged(object sender, EventArgs e)//listbox
+        {
+            
+
+        }
+
+        private void lst_ausgabe_MouseDoubleClick(object sender, EventArgs e)//Eventhandler bestimmtes pdf auslesen.
+        {
+            name = lst_ausgabe.GetItemText(lst_ausgabe.SelectedItem);
+            axAcroPDF1.LoadFile(pfad+ '/' +name);
+        }
+
+        private void VAT_Validator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_validate_Click(object sender, EventArgs e)
+        {
+            
+            
+            Regex vatnr = new Regex(@"\b(?!ATU68205701)\b\b[A-Z][A-Z][A-Z,0-9]\d{8}\b");//\b(?!)\b-> außer 
+            // Regex land = new Regex(@"\b[A-Z][A-Z]\b");
+            //  Regex nummer = new Regex(@"[A-Z,0-9]\d{8}\b");
+
+            vpfad = pfad + '/' + name;
+            PdfReader leser = new PdfReader(@vpfad);
+            ITextExtractionStrategy its = new iTextSharp.text.pdf.parser.LocationTextExtractionStrategy();
+            
+            using (PdfReader reader = new PdfReader(vpfad))
+            {
+
+
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    string thePage = PdfTextExtractor.GetTextFromPage(reader, i, its);
+                    string[] theLines = thePage.Split('\n');
+                    foreach (var theLine in theLines)
+                    {                        
+                        auslesen.AppendLine(theLine);
+                    }
+                }
+            }
+            
+            foreach (Match m in vatnr.Matches(auslesen.ToString())) 
+            {
+                txt_validate.Clear();
+                txt_validate.Text += m.ToString() + Environment.NewLine;
+            }
+            
+
+
+        }
+    }
+}
+
